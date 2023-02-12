914e3b21ff08275468587ce77bb83b39db848733
Benneth93 <58491492+Benneth93@users.noreply.github.com>
2023-02-12 17:12:20 +0000

    removed some warnings

--- RandomDeckGenerator/Pages/DeckInput.cshtml.cs
+++ RandomDeckGenerator/Pages/DeckInput.cshtml.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
-using RandomDeckGenerator.Services;
-using HostingEnvironmentExtensions = Microsoft.AspNetCore.Hosting.HostingEnvironmentExtensions;
 
 namespace RandomDeckGenerator.Pages;
 
